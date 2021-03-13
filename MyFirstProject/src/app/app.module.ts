6c2bd4314bc2d90ded2c15cbf7fc40bcbe65393c
Katherine Aguilar <kaguilar31@misena.edu.co>
2021-03-12 21:39:39 -0500

    Creación de Componentes y Card

--- MyFirstProject/src/app/app.module.ts
+++ MyFirstProject/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContentComponent } from './components/content/content.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
